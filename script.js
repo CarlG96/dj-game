2b1df84ccdb4329b7ae732b4555dcbc9a04187b6
Denys <heartbreathing19951014@gmail.com>
2022-11-17 21:47:35 +0000

    Added some js for music-notes parralax

--- script.js
+++ script.js
@@ -10,3 +10,12 @@ startButton.addEventListener('click', startGame);
 function startGame() {
     popup.classList.add('hide');
 }
+
+// Music notes paralax
+let bg = document.querySelector('.parallax');
+window.addEventListener('mousemove', function (e) {
+    let x = e.clientX / window.innerWidth;
+    let y = e.clientY / window.innerHeight;
+    bg.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
+
+});
