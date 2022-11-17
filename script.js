7b70cb1ed2b3d8932b7f6eb5b8950079325f4a1c
JJBR13 <jjbrown1308@gmail.com>
2022-11-17 16:25:27 +0000

    Add onload keyword

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const popup = document.getElementById('instruc-popup');
 const startButton = document.getElementById('start-game');
 
+window.onload = popup
+
 startButton.addEventListener('click', startGame);
 
 function startGame() {
