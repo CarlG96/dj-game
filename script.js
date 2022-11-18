1e3fae48bc38f4695aef5524375743586e952c7d
CarlG96 <carl.gardiner1@hotmail.com>
2022-11-18 13:54:04 +0000

    added rest of downpress logic, added animation to score counter and added some comment in js

--- script.js
+++ script.js
@@ -25,20 +25,26 @@ function startGame() {
 
 scoreCounter = document.getElementById('score-counter');
 
+// function for adding score, currently unused
+
 const addScore = () => {
     scoreCounter.innerText = parseInt(scoreCounter.innerText) + 1;
 }
 
+//hotkey functions
+
 hotkeys('space', () => {
     mainSong.play();
 })
+
+//button press functions, should be made into single function instead of twelve
 
 hotkeys('q', () => {
     audioQ.play();
     gameButtons[0].classList.add('press');
     setTimeout(() => {
         gameButtons[0].classList.remove('press');
-    }, 150);
+    }, 100);
    
 })
 
@@ -47,7 +53,7 @@ hotkeys('w', () => {
     gameButtons[1].classList.add('press');
     setTimeout(() => {
         gameButtons[1].classList.remove('press');
-    }, 150);
+    }, 100);
 })
 
 hotkeys('e', () => {
@@ -55,41 +61,77 @@ hotkeys('e', () => {
     gameButtons[2].classList.add('press');
     setTimeout(() => {
         gameButtons[2].classList.remove('press');
-    }, 150);
+    }, 100);
 })
 
 hotkeys('r', () => {
     audioR.play();
+    gameButtons[3].classList.add('press');
+    setTimeout(() => {
+        gameButtons[3].classList.remove('press');
+    }, 100);
 })
 
 hotkeys('a', () => {
-    audioA.play();  
+    audioA.play(); 
+    gameButtons[4].classList.add('press');
+    setTimeout(() => {
+        gameButtons[4].classList.remove('press');
+    }, 100); 
 })
 
 hotkeys('s', () => {
     audioS.play();
+    gameButtons[5].classList.add('press');
+    setTimeout(() => {
+        gameButtons[5].classList.remove('press');
+    }, 100);
 })
 
 hotkeys('d', () => {
     audioD.play();
+    gameButtons[6].classList.add('press');
+    setTimeout(() => {
+        gameButtons[6].classList.remove('press');
+    }, 100);
 })
 
 hotkeys('f', () => {
     audioF.play();
+    gameButtons[7].classList.add('press');
+    setTimeout(() => {
+        gameButtons[7].classList.remove('press');
+    }, 100);
 })
 
 hotkeys('z', () => {
     audioZ.play();
+    gameButtons[8].classList.add('press');
+    setTimeout(() => {
+        gameButtons[8].classList.remove('press');
+    }, 100);
 })
 
 hotkeys('x', () => {
     audioX.play();
+    gameButtons[9].classList.add('press');
+    setTimeout(() => {
+        gameButtons[9].classList.remove('press');
+    }, 100);
 })
 
 hotkeys('c', () => {
     audioC.play();
+    gameButtons[10].classList.add('press');
+    setTimeout(() => {
+        gameButtons[10].classList.remove('press');
+    }, 100);
 })
 
 hotkeys('v', () => {
     audioV.play();
+    gameButtons[11].classList.add('press');
+    setTimeout(() => {
+        gameButtons[11].classList.remove('press');
+    }, 100);
 })
