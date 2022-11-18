94a624a20f4fe6be3621321a20e2c7e739e0dcb4
CarlG96 <carl.gardiner1@hotmail.com>
2022-11-18 12:13:48 +0000

    adding sound files

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const popup = document.getElementById('instruc-popup');
 const startButton = document.getElementById('start-game');
+const audioQ = document.getElementById('audioQ');
+const audioW = document.getElementById('audioW');
+const audioE = document.getElementById('audioE');
+const audioR = document.getElementById('audioR');
+const audioA = document.getElementById('audioA');
+const audioS = document.getElementById('audioS');
+const audioD = document.getElementById('audioD');
+const audioF = document.getElementById('audioF');
+const audioZ = document.getElementById('audioZ');
+const audioX = document.getElementById('audioX');
+const audioC = document.getElementById('audioC');
+const audioV = document.getElementById('audioV');
 
 // startButton.addEventListener('click', startGame);
 
@@ -9,51 +21,54 @@ function startGame() {
 
 scoreCounter = document.getElementById('score-counter');
 
-hotkeys('q', () => {
-    console.log("add score")
+const addScore = () => {
     scoreCounter.innerText = parseInt(scoreCounter.innerText) + 1;
+}
+
+hotkeys('q', () => {
+    audioQ.play();
 })
 
 hotkeys('w', () => {
-    
+    audioW.play();
 })
 
 hotkeys('e', () => {
-    
+    audioE.play();
 })
 
 hotkeys('r', () => {
-    
+    audioF.play();
 })
 
 hotkeys('a', () => {
-    
+    audioA.play();  
 })
 
 hotkeys('s', () => {
-    
+    audioS.play();
 })
 
 hotkeys('d', () => {
-    
+    audioD.play();
 })
 
 hotkeys('f', () => {
-    
+    audioF.play();
 })
 
 hotkeys('z', () => {
-    
+    audioZ.play();
 })
 
 hotkeys('x', () => {
-    
+    audioX.play();
 })
 
 hotkeys('c', () => {
-    
+    audioC.play();
 })
 
 hotkeys('v', () => {
-    
+    audioV.play();
 })
