d1d29d734889aeb2b209f4aaeb44e9196574f8fc
Keis Smit <kgsmit98@gmail.com>
2022-11-19 09:12:33 +0200

    temp commit

--- script.js
+++ script.js
@@ -17,13 +17,13 @@ const gameButtons = document.getElementsByClassName('square');
 const audio = document.createElement('audio');
 
 // parralax for music notes
-let bg = document.querySelector('.parallax');
-window.addEventListener('mousemove', function (e) {
-    let x = e.clientX / window.innerWidth;
-    let y = e.clientY / window.innerHeight;
-    bg.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
+// let bg = document.querySelector('.parallax');
+// window.addEventListener('mousemove', function (e) {
+//     let x = e.clientX / window.innerWidth;
+//     let y = e.clientY / window.innerHeight;
+//     bg.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
 
-});
+// });
 
 
 
